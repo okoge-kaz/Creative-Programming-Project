4c8fe6fabee0de2a5ad16c97b7964ac8f16ee509
kazuki <kaz.tokyo.tech20@gmail.com>
2021-04-25 22:01:21 +0900

    4/25のぶん、CPCTFで疲れたので

---
+++ abc193c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const long long INF = 1LL << 60;
+const int inf = (1 << 30);
+const ll mod = 998244353;
+const ll MOD = 1000000007;
+const ld PI = acos(-1.0L);
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+int main(){
+    ll N;
+    cin >> N;
+    map<ll,ll>nums;
+    for(ll i=2;i*i<=N;i++){
+        ll n = i*i;
+        while(n <= N){
+            nums[n]++;
+            n *= i;
+        }
+    }
+    ll cnt = N - nums.size();
+    cout << cnt << endl;
+}
