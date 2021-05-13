5f7ee6218f3983cfd8ab173adfb7c3ae9088c5b9
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-13 15:25:20 +0900

    5/13

---
+++ AOJ/DSL/UnionFind/DSL_1_B.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const long long INF = 1LL << 60;
+const int inf = (1 << 30);
+const ll mod = 998244353;
+const ll MOD = 1000000007;
+const ld PI = acos(-1.0L);
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class Abel> struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<Abel> diff_weight;
+
+    UnionFind(int n = 1, Abel SUM_UNITY = 0) {
+        init(n, SUM_UNITY);
+    }
+
+    void init(int n = 1, Abel SUM_UNITY = 0) {
+        par.resize(n); rank.resize(n); diff_weight.resize(n);
+        for (int i = 0; i < n; ++i) par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+    }
+
+    int root(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        else {
+            int r = root(par[x]);
+            diff_weight[x] += diff_weight[par[x]];
+            return par[x] = r;
+        }
+    }
+
+    Abel weight(int x) {
+        root(x);
+        return diff_weight[x];
+    }
+
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y, Abel w) {
+        w += weight(x); w -= weight(y);
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y), w = -w;
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        diff_weight[y] = w;
+        return true;
+    }
+
+    Abel diff(int x, int y) {
+        return weight(y) - weight(x);
+    }
+};
+int main(){
+    // 重みつきUnion-Find
+    // この記事を参考 https://qiita.com/drken/items/cce6fc5c579051e64fab
+    int N,q;
+    cin >> N >> q;
+    UnionFind<ll> uf(N);
+    for(int i=0;i<q;i++){
+        int t;
+        cin >> t ;
+        if(t==0){
+            int x,y,z;
+            cin >> x >> y >> z;
+            uf.merge(x,y,z);
+        }
+        else{
+            int x, y;
+            cin >> x >> y;
+            if(uf.issame(x,y)){
+                ll diff = uf.diff(x,y);
+                cout << diff << "\n";
+            }
+            else cout << "?\n"; 
+        }
+    }
+}
---
+++ 未分類/abc087d.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const long long INF = 1LL << 60;
+const int inf = (1 << 30);
+const ll mod = 998244353;
+const ll MOD = 1000000007;
+const ld PI = acos(-1.0L);
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class Abel> struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<Abel> diff_weight;
+
+    UnionFind(int n = 1, Abel SUM_UNITY = 0) {
+        init(n, SUM_UNITY);
+    }
+
+    void init(int n = 1, Abel SUM_UNITY = 0) {
+        par.resize(n); rank.resize(n); diff_weight.resize(n);
+        for (int i = 0; i < n; ++i) par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+    }
+
+    int root(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        else {
+            int r = root(par[x]);
+            diff_weight[x] += diff_weight[par[x]];
+            return par[x] = r;
+        }
+    }
+
+    Abel weight(int x) {
+        root(x);
+        return diff_weight[x];
+    }
+
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y, Abel w) {
+        w += weight(x); w -= weight(y);
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y), w = -w;
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        diff_weight[y] = w;
+        return true;
+    }
+
+    Abel diff(int x, int y) {
+        return weight(y) - weight(x);
+    }
+};
+int main(){
+    int N, M;
+    cin >> N >> M;
+    UnionFind<ll> uf(N);
+    for(int i=0;i<M;i++){
+        ll L, R, D;
+        cin >> L >> R >> D;
+        L--,R--;
+        if(uf.issame(L,R)){
+            if(uf.diff(L,R)==D) continue;
+            cout << "No" << "\n";
+            return 0;
+        }
+        else{
+            uf.merge(L,R,D);
+        }
+    }
+    cout << "Yes\n";
+}
