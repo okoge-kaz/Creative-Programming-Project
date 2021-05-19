3c04e63dd67b4aab06ac428b36403f00b5bd8aac
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-19 23:43:13 +0900

    Dijkstra法

--- グラフ系統/最短経路問題/abc035d.cpp
+++ グラフ系統/最短経路問題/abc035d.cpp
@@ -19,7 +19,7 @@ struct edge {
 };
 using Graph = vector<vector<edge>>;
 
-// 経路復元
+// 経路復元用途
 vector<ll> get_path(const vector<ll> &prev, ll g){
     // 方針としては、終点g から始点 sまで逆に辿る
     vector<ll> path;
@@ -32,30 +32,30 @@ vector<ll> get_path(const vector<ll> &prev, ll g){
 }
 
 int main(){
-    // 頂点数、辺数、始点
-    int N, M, s;
-    cin >> N >> M >> s;
-    // 終点がある場合は、下の最短経路復元用の関数の上のg (=終点)を編集すること
     
-    // グラフ
+    ll N, M, T;
+    cin >> N >> M >> T;
+    vector<ll>A(N);
+    for(int i=0;i<N;i++) cin >> A[i];
+
+    // 行って戻ってくるタイプの最短経路問題の時には、辺の向きを逆にするという手法が取られる。
     Graph G1(N);
+    Graph G2(N);
     for(int i=0;i<M;i++){
         int a,b,w;
         cin >> a >> b >> w;
-        // 0-indexed --a,--b;
-        G1[a].push_back(edge(a,b,w));// 有向グラフなので
+        --a,--b;
+        G1[a].push_back(edge(a,b,w));
+        G2[b].push_back(edge(b,a,w));
     }
 
     // ダイクストラ法
     vector<ll> dist1(N,INF);
-    dist1[s] = 0;// 始点
-    vector<ll> prev1(N,-1);// 経路復元
+    dist1[0] = 0;// 始点
+    vector<ll> prev(N,-1);// 経路復元
     
-    // (d[v], v)のペアを要素としたヒープをつくる
     priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> que1;
-    // priority_queue は通常、大きい順になるので、greaterによって逆順にしている
-    // vector<pair<ll,int>> は逆順にするときに必要な処理:詳細はAPG4b
-    que1.push(make_pair(dist1[s], s));
+    que1.push(make_pair(dist1[0], 0));
 
     // ダイクストラ法の反復を開始
     while(!que1.empty()){
@@ -74,20 +74,44 @@ int main(){
                 // 更新があるならヒープに新たに挿入
                 que1.push(make_pair(dist1[e.to],e.to));
                 // 経路復元用
-                prev1[e.to] = v;// vを通って e.toに辿り着いた
+                prev[e.to] = v;// vを通って e.toに辿り着いた
             }
         }
     }
 
-    // 結果出力
-    for(int v=0;v<N;v++){
-        if(dist1[v] < INF) cout << dist1[v] << endl;
-        else cout << "INF" << endl;
+    // ダイクストラ法
+    vector<ll> dist2(N,INF);
+    dist2[0] = 0;// 始点
+    
+    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> que2;
+    que2.push(make_pair(dist2[0], 0));
+
+    // ダイクストラ法の反復を開始
+    while(!que2.empty()){
+        // v: 使用済みでない頂点のうち d[v]が最小
+        // d: vに対するキー値
+        int v = que2.top().second;
+        ll d = que2.top().first;
+        que2.pop();
+
+        // d > dist[v] は、(d,v)がゴミであることを表す = 最短経路でないもの
+        if(d > dist2[v]) continue;
+
+        // 頂点v を始点とした各辺を緩和
+        for(auto e:G2[v]){
+            if(chmin(dist2[e.to],dist2[v] + e.w)){
+                // 更新があるならヒープに新たに挿入
+                que2.push(make_pair(dist2[e.to],e.to));
+            }
+        }
     }
 
-    // 最短経路復元
-    // ゴールをg とする。
-    ll g;
-    auto ans = get_path(prev1, g);// ans は vector 
-    // prev は前に定義した vector , gはgoal
+    vector sum(N,0LL);
+    ll ans = 0;
+    for(int i=0;i<N;i++){
+        sum[i] = max(T - dist1[i] - dist2[i],0LL) * A[i];
+        chmax(ans,sum[i]);
+    }
+    cout << ans << endl;
+    
 }
